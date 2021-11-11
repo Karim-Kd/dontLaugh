3d031bc1e8b5ff2059b0c24a1eea1dab4f5399aa
karkosss <karimkdaiem@gmail.com>
2021-11-11 21:31:17 +0100

    first prototype

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using dontLaugh.Models;
+
+namespace dontLaugh.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<dontLaugh.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dontLaugh.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        public Joke()
+        {
+
+        }
+    }
+}
